0ac9bfb3e31607db625ff6d8c9886f46d378ae1e
Martin <mmaina290@gmail.com>
2024-02-14 21:14:35 +0300

    rendering tasks

--- to_do/script.js
+++ to_do/script.js
@@ -1,9 +1,17 @@
 const tasks = [];
 
 function addTask() {
-    const taskInput = document.getElementById("new-task-input");
+    const taskInput = document.getElementById("taskInput");
     const task = taskInput.value.trim();
     if (task !== "") {
+        // Add the task to the array
+        const task = {
+            id: Date.now(),
+            text:taskText,
+            name: task,
+            completed: false,
+            timestamp: new Date().toLocaleString()
+        };
         tasks.push(task);
         taskInput.value = "";
         renderTasks();
