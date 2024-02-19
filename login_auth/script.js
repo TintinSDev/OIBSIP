d1ab5994aa2182f3a50ca4386596c0f4818f5019
Martin <mmaina290@gmail.com>
2024-02-19 20:53:46 +0300

    login in progress

---
+++ login_auth/script.js
@@ -0,0 +1,29 @@
+
+
+    //wrapper event handlers
+    const wrapper = document.querySelector('.wrapper');
+    const loginLink = document.querySelector('.login-link');
+    const registerLink = document.querySelector('.register-link');
+    const btnPopup = document.querySelector('.btnLogin-popup');
+    const iconClose = document.querySelector('.icon-close');
+    const wrapperForm = document.querySelector('.wrapper-form');
+    const addFeedback = document.querySelector('.add-feedback');
+
+
+
+    registerLink.addEventListener('click', () => {
+        wrapper.classList.add('active');
+    });
+
+    loginLink.addEventListener('click', () => {
+        wrapper.classList.remove('active');
+    });
+
+    btnPopup.addEventListener('click', () => {
+    wrapper.classList.add('active-popup');
+    });
+
+    iconClose.addEventListener('click', () => {
+        wrapper.classList.remove('active-popup');
+    });
+   
